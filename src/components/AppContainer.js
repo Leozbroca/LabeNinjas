59c6e8f225c1fff01a2525fe3c27213ce916f3f3
pedrohenriqueduarte <ph09duarte@gmail.com>
2021-11-25 10:52:22 -0300

    filtros, ajustes e termino das funcionalidades

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -5,103 +5,100 @@ import Carrinho from './Carrinho'
 import axios from 'axios'
 
 export class AppContainer extends Component {
-  state={
+  state = {
     page: "home",
     listaServicos: [],
   }
 
-  componentDidMount = () => {
-    this.getAllJobs()
-  }
 
   togglePage = () => {
-    switch (this.state.page){
+    switch (this.state.page) {
       case "home":
         return <p>Pronto para começar</p>
       case "queroSerUmNinja":
-        return <Cadastro/>
+        return <Cadastro />
       case "contrateUmServico":
         return <Servicos
-        listaServicos={this.state.listaServicos}
-        getAllJobs={this.getAllJobs}
-        adicionarAoCarrinho={this.adicionarAoCarrinho}
+          listaServicos={this.state.listaServicos}
+          getAllJobs={this.getAllJobs}
+          adicionarAoCarrinho={this.adicionarAoCarrinho}
         />
       case "carrinho":
         return <Carrinho
-        listaServicos={this.state.listaServicos}
-        removendoDoCarrinho={this.removendoDoCarrinho}
+          listaServicos={this.state.listaServicos}
+          removendoDoCarrinho={this.removendoDoCarrinho}
         />
       default:
-        return <appContainer/>
+        return <appContainer />
     }
   }
 
   mudarPaginaCadastro = () => {
-    this.setState({page: "queroSerUmNinja"})
+    this.setState({ page: "queroSerUmNinja" })
   }
 
   mudarPaginaServicos = () => {
-    this.setState({page: "contrateUmServico"})
+    this.setState({ page: "contrateUmServico" })
   }
 
   mudarPaginaCarrinho = () => {
-    this.setState({page: "carrinho"})
+    this.setState({ page: "carrinho" })
   }
 
   adicionarAoCarrinho = (id) => {
     const url = `https://labeninjas.herokuapp.com/jobs/${id}`
     const body = {
-        "taken": true
-      }
+      "taken": true
+    }
 
     axios.post(url, body, {
-        headers: {
-            Authorization: "944276f6-19c0-49d4-ab75-a9d3e31490f9"
-        }
+      headers: {
+        Authorization: "944276f6-19c0-49d4-ab75-a9d3e31490f9"
+      }
     })
-    .then((res) => {
+      .then((res) => {
         alert("Item adicionado ao carrinho!")
         this.getAllJobs()
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         console.log(err.response.data.message)
-    })
+      })
   }
 
   removendoDoCarrinho = (id) => {
     const url = `https://labeninjas.herokuapp.com/jobs/${id}`
     const body = {
-        "taken": false
-      }
+      "taken": false
+    }
 
     axios.post(url, body, {
-        headers: {
-            Authorization: "944276f6-19c0-49d4-ab75-a9d3e31490f9"
-        }
+      headers: {
+        Authorization: "944276f6-19c0-49d4-ab75-a9d3e31490f9"
+      }
     })
-    .then((res) => {
+      .then((res) => {
         alert("Item removido do carrinho!")
         this.getAllJobs()
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         console.log(err.response.data.message)
-    })
+      })
   }
 
   getAllJobs = () => {
     const url = "https://labeninjas.herokuapp.com/jobs"
     axios.get(url, {
-        headers: {
-            Authorization: "944276f6-19c0-49d4-ab75-a9d3e31490f9"
-        }
+      headers: {
+        Authorization: "944276f6-19c0-49d4-ab75-a9d3e31490f9"
+      }
     })
-    .then((res) => {
-        this.setState({listaServicos: res.data.jobs})
+      .then((res) => {
+        this.setState({ listaServicos: res.data.jobs })
         console.log(res)
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         console.log("erro", err)
-    })
+      })
   }
 
   render() {
