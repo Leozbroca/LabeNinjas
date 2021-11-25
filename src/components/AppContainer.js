a9c9b8af257e860e22b9cc8560d6ce02e51b4b56
Igor M <90478654+0dayIg0r@users.noreply.github.com>
2021-11-25 18:23:30 -0300

    Revert "Estilo"

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -4,21 +4,10 @@ import Servicos from './Servicos'
 import Carrinho from './Carrinho'
 import DetalhesServicos from './DetalhesServicos'
 import axios from 'axios'
-import Button from '@material-ui/core/Button';
-import { ThemeProvider } from '@material-ui/core/styles';
-import { theme } from './Temas'
-import Logo from '../IMG/logo.png'
-import FB from '../IMG/fb.png'
-import INSTA from '../IMG/insta.png'
-import TT from '../IMG/tt.png'
-import * as C from './styles'
-
-
-
 
 export class AppContainer extends Component {
   state = {
-    page: "queroSerUmNinja",
+    page: "home",
     listaServicos: [],
     jobDetalhes: {}
   }
@@ -47,16 +36,10 @@ export class AppContainer extends Component {
         return <DetalhesServicos 
         jobDetalhes={this.state.jobDetalhes}
         />
-
       default:
-        return <appContainer/>
+        return <appContainer />
     }
   }
-
-  mudarPaginaHome = () => {
-    this.setState({ page: "home" })
-  }
-
 
   mudarPaginaCadastro = () => {
     this.setState({ page: "queroSerUmNinja" })
@@ -153,61 +136,12 @@ export class AppContainer extends Component {
 
   render() {
     return (
-      <C.Global>
-        <C.GlobalStyle/>
-        <C.FlexHeader>
-          <C.FlexHeader>
-          <C.Img src ={Logo} onClick={this.mudarPaginaHome}/>
-          <h1 onClick={this.mudarPaginaHome}>
-            Labeninjas
-          </h1>
-          </C.FlexHeader>
-
-          <div>
-      <ThemeProvider theme={theme}>
-        <Button variant="contained" color="primary"  onClick={this.mudarPaginaCadastro} >
-          Quero ser um ninja
-        </Button>
-
-        <Button variant="contained" color="primary"  onClick={this.mudarPaginaServicos} >
-           Contrate um serviço
-        </Button>
-
-        <Button variant="contained" color="primary" onClick={this.mudarPaginaCarrinho} >
-            Carrinho
-         </Button>
-         </ThemeProvider>
-         </div>
-         </C.FlexHeader>
-         <div>
-         {this.togglePage()}
-         </div>
-         
-         <C.FooterFlex>
-           <div>
-             <h3>Redes Sociais</h3>
-             <C.Social src = {FB}/>
-             <C.Social src = {INSTA}/>
-             <C.Social src = {TT}/>
-           </div>
-          <C.Canais>
-           <div>
-             <h4>Atendimento:</h4>
-             <p>0800-999-541</p>
-             <p>sac@labeninjas.com</p>
-             </div>
-
-             <div>
-              <h4>Endereço:</h4>
-              <p>Travessa Itatiba, número</p>
-           </div>
-           </C.Canais>
-         </C.FooterFlex>
-         
-        {/* <button onClick={this.mudarPaginaCadastro}>Quero ser um ninja</button>
+      <div>
+        <button onClick={this.mudarPaginaCadastro}>Quero ser um ninja</button>
         <button onClick={this.mudarPaginaServicos}>Contrate um serviço</button>
-        <button onClick={this.mudarPaginaCarrinho}>Carrinho</button> */}
-        </C.Global>
+        <button onClick={this.mudarPaginaCarrinho}>Carrinho</button>
+        {this.togglePage()}
+      </div>
     )
   }
 }
